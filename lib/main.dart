6aad68c30e5aa268a7ae40625db882efcad1e11f
mchocilowicz <marcin.chocilowicz@gmail.com>
2024-03-26 15:15:06 +0100

    pass value from form

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: 'Weight Tracker',
     home: HomeScreen(),
   ));
 }
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-  final formData = GlobalKey<FormState>();
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<double> weightList = [];
 
   @override
   Widget build(BuildContext context) {
-    List<int> weightList = random
-        .amount((i) => random.decimal(min: 40, scale: 80).ceil(), 16, min: 10);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weight Tracker'),
@@ -28,54 +29,18 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.green,
         tooltip: 'Wprowadz nowy pomiar',
         onPressed: () {
-          showModalBottomSheet(
+          showModalBottomSheet<double>(
               isDismissible: false,
               context: context,
               builder: (context) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
-                  child: Form(
-                      key: formData,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextFormField(
-                            keyboardType: const TextInputType.numberWithOptions(
-                                decimal: true),
-                            decoration: const InputDecoration(
-                                hintText: "Wprowadz nowy pomiar",
-                                label: Text("Waga")),
-                            validator: (value) {
-                              if (value == null ||
-                                  value.isEmpty ||
-                                  double.tryParse(value) == null ||
-                                  double.tryParse(value)! <= 0) {
-                                return "Wprowadzona waga jest niepoprawna.";
-                              }
-                              return null;
-                            },
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton(
-                                  onPressed: () {
-                                    if (formData.currentState!.validate()) {
-                                      Navigator.of(context).pop();
-                                    }
-                                  },
-                                  child: const Text("Zapisz")),
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text("Zamknij"))
-                            ],
-                          )
-                        ],
-                      )),
-                );
+                return WeightForm();
+              }).then((value) => {
+                if (value != null)
+                  {
+                    setState(() {
+                      weightList.insert(0, value);
+                    })
+                  }
               });
         },
         child: const Icon(
@@ -94,6 +59,70 @@ class HomeScreen extends StatelessWidget {
                 color: Colors.black,
               ),
           itemCount: weightList.length),
+    );
+  }
+}
+
+class WeightForm extends StatefulWidget {
+  const WeightForm({super.key});
+
+  @override
+  State<WeightForm> createState() => _WeightFormState();
+}
+
+class _WeightFormState extends State<WeightForm> {
+  final formData = GlobalKey<FormState>();
+
+  double? newWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20)
+          .copyWith(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Form(
+          key: formData,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(
+                    hintText: "Wprowadz nowy pomiar", label: Text("Waga")),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      double.tryParse(value) == null ||
+                      double.tryParse(value)! <= 0) {
+                    return "Wprowadzona waga jest niepoprawna.";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  newWeight = double.tryParse(value!);
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        if (formData.currentState!.validate()) {
+                          formData.currentState!.save();
+                          Navigator.of(context).pop(newWeight);
+                        }
+                      },
+                      child: const Text("Zapisz")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Zamknij"))
+                ],
+              )
+            ],
+          )),
     );
   }
 }
